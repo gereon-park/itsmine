f4eaa6fae7037d6d04a4b81c7ac4b14015afb889
gereon-park <mkpark7@gmail.com>
2021-02-22 01:07:12 +0900

    tomcat on kube

---
+++ tomcat_on_kubernetes/getTomcat8.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+echo "Download.. https://downloads.apache.org/tomcat/tomcat-8/v8.5.63/bin/apache-tomcat-8.5.63.tar.gz";
+curl -LO https://downloads.apache.org/tomcat/tomcat-8/v8.5.63/bin/apache-tomcat-8.5.63.tar.gz
+echo;
